a37002712fd3dfec7261f6f44a23efe0f174297c
Andrey <sudarev.andrey.i@gmail.com>
2022-01-25 22:26:43 +0300

    implement logging/history

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -3,14 +3,25 @@ const config = require('../config');
 
 class OrdersController {
   async createOrders(req, res) {
-    const { ORDER_NUM, PLACE_ID } = req.query;
+    const { ORDER_NUM, PLACE_ID, USER_ID } = req.query;
     const pool = await sql.connect(config);
     const response = await pool.request().query(
-      `INSERT INTO [ORDERS] ([ORDER_NUM],[PLACE_ID],[DATE_ADDED]) 
-      OUTPUT inserted.* VALUES
-      ${ORDER_NUM.split(' ').map(
-        (order) => `('${order}',${PLACE_ID}, GETDATE())`
-      )};`
+      `INSERT INTO ORDERS_LOG 
+       OUTPUT inserted.*
+       SELECT 
+       GETDATE() AS DATE_,
+       insertedVal.ORDER_NUM,
+       insertedVal.PLACE_ID,
+       insertedVal.DATE_ADDED,
+       '${USER_ID}' as [USER_ID],
+       'add' as CODE,
+       insertedVal.ID as ORDER_ID
+       FROM
+       (INSERT INTO [ORDERS] ([ORDER_NUM],[PLACE_ID],[DATE_ADDED]) 
+       OUTPUT inserted.* VALUES
+       ${ORDER_NUM.split(' ').map(
+         (order) => `('${order}',${PLACE_ID}, GETDATE())`
+       )}) insertedVal`
     );
     res.json(response.recordset);
   }
@@ -68,24 +79,76 @@ class OrdersController {
     res.json(response.recordset);
   }
 
-  async deleteOrderByOrderID(req, res) {
-    const { ID } = req.query;
+  async getOrdersLog(req, res) {
+    const { GATE_ID } = req.query;
     const pool = await sql.connect(config);
     const response = await pool.request().query(
-      `DELETE FROM [ORDERS]
-      OUTPUT deleted.*
-      WHERE ID = ${ID};`
+      `SELECT 
+      ORDERS_LOG.ID,
+      ORDERS_LOG.DATE_, 
+      ORDERS_LOG.ORDER_NUM, 
+      ORDERS_LOG.DATE_ADDED, 
+      ORDERS_LOG.USER_ID, 
+      ORDERS_LOG.CODE, 
+      ORDERS_LOG.ORDER_ID, 
+      PLACES.PLACE, 
+      PLACES.ID AS PLACE_ID, 
+      GATES.GATE, 
+      GATES.ID AS GATE_ID
+      FROM PLACES 
+      LEFT OUTER JOIN GATES 
+      ON PLACES.GATE_ID = dbo.GATES.ID 
+      LEFT OUTER JOIN ORDERS_LOG 
+      ON PLACES.ID = ORDERS_LOG.PLACE_ID
+      WHERE
+      ORDERS_LOG.ID IS NOT NULL
+      AND DATE_ > GETDATE()-7
+      AND GATES.ID = ${GATE_ID}`
+    );
+
+    res.json(response.recordset);
+  }
+
+  async deleteOrderByOrderID(req, res) {
+    const { ID, USER_ID } = req.query;
+    const pool = await sql.connect(config);
+    const response = await pool.request().query(
+      `INSERT INTO ORDERS_LOG 
+       OUTPUT inserted.*
+       SELECT 
+       GETDATE() AS DATE_,
+       deleted.ORDER_NUM,
+       deleted.PLACE_ID,
+       deleted.DATE_ADDED,
+       '${USER_ID}' as [USER_ID],
+       'delete' as CODE,
+       deleted.ID as ORDER_ID
+       FROM
+          (DELETE FROM [ORDERS]
+          OUTPUT deleted.*
+          WHERE ID = ${ID}) deleted`
     );
     res.json(response.recordset);
   }
 
   async deleteOrderByPlaceID(req, res) {
-    const { PLACE_ID } = req.query;
+    const { PLACE_ID, USER_ID } = req.query;
     const pool = await sql.connect(config);
     const response = await pool.request().query(
-      `DELETE FROM [ORDERS]
-      OUTPUT deleted.*
-      WHERE PLACE_ID = ${PLACE_ID};`
+      `INSERT INTO ORDERS_LOG 
+       OUTPUT inserted.*
+       SELECT 
+       GETDATE() AS DATE_,
+       deleted.ORDER_NUM,
+       deleted.PLACE_ID,
+       deleted.DATE_ADDED,
+       '${USER_ID}' as [USER_ID],
+       'delete' as CODE,
+       deleted.ID as ORDER_ID
+       FROM
+          (DELETE FROM [ORDERS]
+          OUTPUT deleted.*
+          WHERE PLACE_ID = ${PLACE_ID}) deleted`
     );
     res.json(response.recordset);
   }
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -4,6 +4,7 @@ const OrdersController = require('../controllers/orders.controller');
 
 router.post('/orders/order', OrdersController.createOrders);
 router.get('/orders/order', OrdersController.getOneOrder);
+router.get('/orders/log', OrdersController.getOrdersLog);
 router.get('/orders/list', OrdersController.getOrders);
 router.delete('/orders/order', OrdersController.deleteOrderByOrderID);
 router.delete('/orders/place', OrdersController.deleteOrderByPlaceID);
