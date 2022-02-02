23a3710499e9a5f8557be07a72008b793c947f97
Andrey <91373803+S-404@users.noreply.github.com>
2022-02-02 14:22:48 +0300

    upd login.controller.js

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -4,6 +4,7 @@ const LoginController = require('../controllers/login.controller');
 
 router.post('/user', LoginController.createUser);
 router.get('/user/checkpassword', LoginController.checkPassword);
+router.get('/users', LoginController.getUserList);
 router.put('/user/access', LoginController.updateUserAccess);
 router.put('/user/password', LoginController.updateUserPassword);
 router.delete('/user', LoginController.deleteUser);
